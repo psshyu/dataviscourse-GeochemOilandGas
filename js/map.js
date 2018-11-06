b23510d4b0924cffe523f21eb6dfbbac6be8af39
Pablo <pablo.napan@utah.edu>
2018-11-06 16:38:35 -0700

    commit

--- js/map.js
+++ js/map.js
@@ -9,7 +9,15 @@ class Map {
     /**
      * not sure of the exact params that should be passed into the constructor right now
      */
-    constructor(projection) {
+    constructor(projection, geospatialData, tocChart, vanKrevPlot, potentialPlot, inverseKrevPlot) {
+
+        this.projection = projection;
+        this.geospatialData = geospatialData;
+        this.tocChart = tocChart;
+        this.vanKrevPlot = vanKrevPlot;
+        this.potentialPlot = potentialPlot;
+        this.inverseKrevPlot = inverseKrevPlot;
+
         let svg = d3.select("#map")
                     .append("svg")
                         .attr("id", "mapSVG")
@@ -34,11 +42,28 @@ class Map {
              * this.svg.append("path")
              * .attr("d", path( <<BASIN PATHS GO HERE>>));
              */
-            //Not sure how to call my json from here. are d2.json calls nest-able? Will fix later
+            //Not sure how to call my json from here. are d3.json calls nest-able? Will fix later
             this.svg.append("path")
                 .attr("d", path(topojson.feature(us, us.objects.nation)));
             });
+    }
 
+    update(){
+
+            //filter data points falling outside geoAlbers projection extent
+            //get a wider projection as we have offshore wells in the gulf of mexico (sea) and Cali.
+            this.geospatialData = this.geospatialData.filter(d => projection([d.Longitude,d.Latitude])!==null);
+
+            let wells = this.svg.selectAll('circle').data(this.geospatialData);
+            wells.exit().remove();
+            let new_wells = wells.enter().append('circle');
+            wells = new_wells.merge(wells);
+
+            wells
+                .attr('cx', d => this.projection([d.Longitude,d.Latitude])[0])
+                .attr('cy', d => this.projection([d.Longitude,d.Latitude])[1])
+                .attr('r', 2)
+                .style('fill','#87CEFA')
     }
 
 }
@@ -63,50 +88,9 @@ class Basin {
                 .append('path')
 
                 .attr('d', basin_path)
-                .attr('stroke', 'brown');
+                .attr('stroke', 'grey');
                 // .attr('fill', 'grey');
 
         })
-    }
-}
-
-
-
-class Well {
-
-    constructor(projection){
-
-        this.projection = projection;
-        this.svg = d3.select("#mapSVG");
-
-        d3.csv('data/SRCPhase2GeospatialUSA2.csv', (error, well_geospatial) => {
-
-
-            //filter data points falling outside geoAlbers projection extent
-            //get a wider projection as we have offshore wells in the gulf of mexico (sea) and Cali.
-            well_geospatial = well_geospatial.filter(d => projection([d.Longitude,d.Latitude])!==null);
-
-            let wells = this.svg.selectAll('circle').data(well_geospatial);
-            wells.exit().remove();
-            let new_wells = wells.enter().append('circle');
-            wells = new_wells.merge(wells);
-
-            wells
-                .attr('cx', d => projection([d.Longitude,d.Latitude])[0])
-                .attr('cy', d => projection([d.Longitude,d.Latitude])[1])
-                .attr('r', 2)
-                .style('fill','#87CEFA')
-
-
-        })
-}
-
-    update(data){
-
-
-
-
-
-
     }
 }
