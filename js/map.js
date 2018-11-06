68ea6abd4a8bad5f6bff016e4775e050784923fd
Pablo <pablo.napan@utah.edu>
2018-11-05 22:15:04 -0700

    basins showing up on map. Projections are different, though

--- js/map.js
+++ js/map.js
@@ -22,8 +22,13 @@ class Map {
                         .attr("vertical-align", "middle")
                         .attr("horizontal-align", "middle");
 
+
         this.svg = d3.select("#mapSVG");
         let path = d3.geoPath();
+
+        this.projection = d3.geoAlbersUsa();
+
+
         d3.json("https://unpkg.com/us-atlas@1/us/10m.json", (error, us) => {
             if (error) throw error;
             /**
@@ -31,9 +36,30 @@ class Map {
              * this.svg.append("path")
              * .attr("d", path( <<BASIN PATHS GO HERE>>));
              */
+            //Not sure how to call my json from here. Is it nest-able? Will fix later
             this.svg.append("path")
                 .attr("d", path(topojson.feature(us, us.objects.nation)));
             });
+
+
+        d3.json("data/USGS_Provinces_topo.json", (error, basins) => {
+
+            let geojson = topojson.feature(basins, basins.objects.USGS_Provinces);
+            let geofeatures = geojson.features;
+            let basin_path = d3.geoPath().projection(this.projection);
+
+            this.svg.selectAll('path').data(geofeatures)
+                .enter()
+                .append('path')
+
+                .attr('d',basin_path)
+                .attr('stroke','brown')
+                .attr('fill','grey');
+
+        console.log(geojson);
+        });
+
+
 
         console.log("done rendering map");
     }
