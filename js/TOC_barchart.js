64365e65674033a9aff375cf95bbe945b8f03290
Pablo <pablo.napan@utah.edu>
2018-11-18 02:57:51 -0700

    histogram showing up, still need to work on the bin size and axes

--- js/TOC_barchart.js
+++ js/TOC_barchart.js
@@ -43,63 +43,93 @@ class TOC_barchart {
 
         let tocValues = data.filter(d => d.TOC_Percent_Measured !== '');
         tocValues = tocValues.map(function(d){ return parseFloat(d.TOC_Percent_Measured)});
+
         console.log(tocValues);
 
-        let minToc = d3.min(tocValues);
-        let maxToc = d3.max(tocValues);
-        console.log(minToc,maxToc);
+        if (tocValues.length > 0) {
+
+            this.svg.select('#noInfo').remove();
+
+            let minToc = d3.min(tocValues);
+            let maxToc = d3.max(tocValues);
 
 
-        let xScale = d3.scaleLinear()
-            .domain([minToc, maxToc])
-            .range([this.margin.left, this.width-this.margin.right]);
+            let xScale = d3.scaleLinear()
+                .domain([minToc, maxToc])
+                .range([this.margin.left, this.width - this.margin.right]);
 
-        //building histogram holder. Considering adaptive toc domain if max toc < 10, else cut the data display off at 10
-        let histogram_= null;
-        if (maxToc > 10) {
+            //building histogram holder. Considering adaptive toc domain if max toc < 10, else cut the data display off at 10
+            let histogram_ = null;
+            if (maxToc > 10) {
 
                 histogram_ = d3.histogram()
-                .domain([0, 10])
-                .thresholds(xScale.ticks(10));
+                    .domain([0, 10])
+                    .thresholds(xScale.ticks(9));
+            } else {
+                histogram_ = d3.histogram()
+                    .domain([0, maxToc])
+                    .thresholds(xScale.ticks(9));
+            }
+
+            //building histogram
+            let histogram = histogram_(tocValues);
+            console.log(histogram);
+
+            //yScale
+            let maxCount = d3.max(histogram.map(d => d.length));
+            let yScale = d3.scaleLinear()
+                .domain([0,maxCount])
+                .range([this.margin.bottom, this.height - this.margin.top]);
+
+
+
+            let bars = this.svg.selectAll('.bar').data(histogram);
+
+            bars.exit()
+                .attr("opacity", 1)
+                // .transition()
+                // .duration(1000)
+                .attr("opacity", 0)
+                .remove();
+
+            let newBars = bars.enter().append('rect');
+
+            bars = newBars.merge(bars);
+
+            bars
+                .attr('class','bar')
+                .attr('x', d => xScale(+d.x0))
+                .attr('y', 0)
+                .attr('width', 10)
+                .attr('height', d => {
+                    // console.log(d.length);
+                    return yScale(+d.length)})
+                .attr('opacity',0.5)
+                .style('fill','grey')
+                .style('stroke','black')
+                .attr("transform", "translate(300,250), rotate(180)");
+
+            // .transition()
+                // .duration(1000);
+
+            //
+            // let xAxis = d3.axisBottom(xScale);
+            //
+            // this.svg.append("g")
+            //     .call(xAxis);
+            // let yAxis = d3.axisLeft(yScale);
+            //
+            // this.svg.append("g")
+            //     .call(yAxis);
+
         }else{
-                histogram_ = d3.histogram()
-                .domain([0, maxToc])
-                .thresholds(xScale.ticks(10));
+            this.svg.selectAll('.bar').remove();
+            this.svg.append('text')
+                .attr('x',this.width/2)
+                .attr('y',this.height/2)
+                .text('No information available')
+                .attr('id','noInfo')
         }
-
-        //building histogram
-        let histogram = histogram_(tocValues);
-        console.log(histogram);
-
-        //yScale
-        let maxCount = d3.max(histogram.map(d => d.length));
-        let yScale = d3.scaleLinear()
-            .domain([maxCount, 0])
-            .range([this.margin.bottom, this.height-this.margin.top]);
-
-
-        let bars = this.svg.selectAll('.bar').data(histogram);
-        bars.exit().remove();
-        let newBars = bars.enter().append('rect');
-        bars = newBars.merge(bars);
-
-        bars
-            .attr('x', d => xScale(+d.x0))
-            .attr('y', 0)
-            .attr('width', d => xScale(+d.x1 - +d.x0))
-            .attr('height', d => yScale(+d.length));
-
-
-        let xAxis = d3.axisBottom(xScale);
-
-        this.svg.append("g")
-            .call(xAxis);
-        let yAxis = d3.axisLeft(yScale);
-
-        this.svg.append("g")
-            .call(yAxis);
-
-
 
         //
         // let xScale = d3.scaleLinear()
