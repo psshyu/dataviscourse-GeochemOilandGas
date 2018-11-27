72e4bb6ba2964b4115cacfa93293a585bd9deb61
Skylar Shyu <skylarshyu@skylars-mbp.uconnect.utah.edu>
2018-11-27 15:31:45 -0700

    fixed inverse krev's axis and point hoverover

--- js/inverseKrevelen_plot.js
+++ js/inverseKrevelen_plot.js
@@ -2,7 +2,29 @@
 // upper right in reference image
 
 class InverseKrevelen{
+    mouseOverHandler(d, i){
+        let id = d.SRCLocationID+"iVKTip";
+        d3.select("#inverseKrevPlot")
+            .append("div")
+            .style("left", d3.event.pageX + 15+"px")
+            .style("top", d3.event.pageY+ 15+"px")
+            .style("padding", "5px 5px 5px 5px")
+            .style("position", "absolute")
+            .style("z-index", 10)
+            .style("background-color", d3.rgb(255,255,255,0.8))
+            .style("border", "1px solid black")
+            .attr("id", id)
+            .html(() => { 
+                return "<h6>" + d.SRCLocationID + "</h6>"
+                    + "<text style='text-align: left;'>Hydrogen Index: "+ d.Hydrogen_Index+"</text>"
+                    + "<br>"
+                    + "<text style='text-align: left;'>Tmax: "+ d.Tmax_C_Pyrolysis+"</text>"; });
+    }
 
+    mouseOutHandler(d, i) {
+        let id = d.SRCLocationID+"iVKTip";
+        d3.select("#"+id).remove();
+    }
     constructor(defaultData, defaultFormation, wellDetails){
         this.defaultData = defaultData;
         this.defaultFormation = defaultFormation;
@@ -10,7 +32,7 @@ class InverseKrevelen{
 
         this.margin = {top: 30, right: 30, bottom: 30, left: 30};
         this.width = document.documentElement.clientWidth* 0.30;
-        this.height = document.documentElement.clientHeight * 0.30;
+        this.height = document.documentElement.clientHeight * 0.45;
 
         this.svg = d3.select("#inverseKrevPlot")
             .append("svg")
@@ -27,28 +49,19 @@ class InverseKrevelen{
         //filter out data that lacks HI && OI
         let samplesWithInformation = defaultData.filter(d => {if (d.Hydrogen_Index !== '' && d.Tmax_C_Pyrolysis !== '') return d});
 
-        //get minimum and maximum values of HI
-        let minmaxHI = this.minmax(samplesWithInformation,'Hydrogen_Index');
-        let minHI = minmaxHI[0];
-        let maxHI = minmaxHI[1];
-        
-        //get minimum and maximum values of OI
-        let minmaxOI = this.minmax(samplesWithInformation,'Tmax_C_Pyrolysis');
-        let minOI = minmaxOI[0];
-        let maxOI = minmaxOI[1];
 
         // X and Y scales 
         this.x = d3.scaleLinear()
-                .domain([maxOI, minOI])
+                .domain([500, 380])
                 .range([this.width - this.margin.right, this.margin.left*2]);
         this.y = d3.scaleLinear()
-                .domain([minHI,maxHI])
-                .range([this.height - this.margin.bottom,this.margin.top]);
+                .domain([0,900])
+                .range([this.height - this.margin.bottom*2, this.margin.top *2]);
 
         //y gridlines
         this.svg.append("g")
             .attr("class", "grid")
-            .attr("transform", "translate("+ this.margin.right *2+ "," + 8 + ") scale(0.84,1)")
+            .attr("transform", "translate("+ this.margin.right *2+ "," + 0 + ") scale(0.79,1)")
             .call(d3.axisLeft(this.y)
                 .tickSize(-this.width, 0, 0)
                 .tickFormat("")
@@ -57,20 +70,20 @@ class InverseKrevelen{
         // X-axis
         this.svg.append("g")
             .attr("id", "invKrevPlotX")
-            .attr("transform", "translate(0," + 270 + ")")
+            .attr("transform", "translate(0," + parseInt(this.height - this.margin.bottom*2) + ")")
             .call(d3.axisBottom(this.x));
       
         // Y Axis
         this.svg.append("g")
             .attr("id", "invKrevPlotY")
-            .attr("transform", "translate("+ this.margin.right*2 + "," + 8 + ")")
+            .attr("transform", "translate("+ this.margin.right * 2 + "," + 0 + ")")
             .call(d3.axisLeft(this.y));
 
         // Axis labels
         // x
         this.svg.append("text")
             .attr("x", this.width/2.25)
-            .attr("y", this.height + this.margin.bottom/2)
+            .attr("y", parseInt(this.height - this.margin.bottom))
             .text("Tmax");
         // y
         this.svg.append('text')
@@ -94,56 +107,13 @@ class InverseKrevelen{
                 return color;})
             .attr("stroke", "gray")
             .attr("r", 5)
-            .style("opacity", 1);
+            .style("opacity", 1)
+            .on("mouseover", this.mouseOverHandler)
+            .on("mouseout", this.mouseOutHandler);
     }
-    /**
-     * 
-     * gets the min and max values of an object, when given a list of the objects.
-     */
-    minmax(samples, tag){
-        
-        if(samples.length > 0){
-            let min = parseFloat(samples[0][tag]);
-            let max = parseFloat(samples[0][tag]);
-            for(let i = 0; i < samples.length; i++){
-                let currentValue = parseFloat(samples[i][tag]);
-                if(currentValue < min){
-                    min = currentValue;
-                }
-                if(currentValue > max){
-                    max = currentValue;
-                }
-            }
-            return [min, max];
-        }
-        else{
-            return[0,10];
-        }
-    }
-    
+
     update(samples, wellDetails){
         let samplesWithInformation = samples.filter(d => {if (d.Hydrogen_Index !== '' && d.Tmax_C_Pyrolysis !== '') return d});
-        //get minimum and maximum values of HI
-        let minmaxHI = this.minmax(samplesWithInformation,'Hydrogen_Index');
-        let minHI = minmaxHI[0];
-        let maxHI = minmaxHI[1];
-        
-        //get minimum and maximum values of OI
-        let minmaxOI = this.minmax(samplesWithInformation,'Tmax_C_Pyrolysis');
-        let minOI = minmaxOI[0];
-        let maxOI = minmaxOI[1];
-
-        //transition the X-axis
-        this.x.domain([maxOI, minOI]);
-        this.svg.select("#invKrevPlotX")
-            .transition()
-            .call(d3.axisBottom(this.x));
-
-        //transition the Y-axis
-        this.y.domain([minHI,maxHI])
-        this.svg.select("#invKrevPlotY")
-            .transition()
-            .call(d3.axisLeft(this.y));
 
         let c = this.svg.selectAll("circle").data(samplesWithInformation);
         c.exit().remove();
@@ -152,13 +122,12 @@ class InverseKrevelen{
 
         this.svg.selectAll("circle")
             .transition()
-            .duration(1000)
+            .duration(900)
             .attr("id", (d)=>{return d.SRCLocationID})
             .attr("fill", (d,i) => {
                 let color;
                 wellDetails.forEach( well => {
                     if(d.SRCLocationID === well.wellID){
-                        //console.log(well.color)
                         color = well.color;
                     }})
                 return color;
@@ -177,13 +146,26 @@ class InverseKrevelen{
                     return color;
                 });
             });
-        this.svg.selectAll("circle").attr("stroke", "gray").attr("r", 5).style("opacity", 1);
+        this.svg.selectAll("circle")
+                .attr("stroke", "gray")
+                .attr("r", 5)
+                .style("opacity", 1)
+                .on("mouseover", this.mouseOverHandler)
+                .on("mouseout", this.mouseOutHandler);
     }
     updateWells(selectedWells){
-        this.svg.selectAll("circle").style("opacity", 0.25).attr("stroke", "white");
+        this.svg.selectAll("circle")
+                .style("opacity", 0.25)
+                .attr("stroke", "white")
+                .on("mouseover", null)
+                .on("mouseout", null);
         selectedWells.forEach(id => {
             this.svg.selectAll("#"+id).raise(); //brings the selected elements to the top
-            this.svg.selectAll("#"+id).style("opacity", 1).attr("stroke", "black");
+            this.svg.selectAll("#"+id)
+                    .style("opacity", 1)
+                    .attr("stroke", "black")
+                    .on("mouseover", this.mouseOverHandler)
+                    .on("mouseout", this.mouseOutHandler);
         });
     }
 }
